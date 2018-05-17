b60c926ec5c9bf32f8316c9a4e3c39150511415a
Sonik <dylan.lemaire@icloud.com>
2018-05-17 20:02:02 +0200

    Ajout de la page du menu principale

--- public/dashboard.php
+++ public/dashboard.php
@@ -4,16 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="favicon.png" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <!-- Chargement de la grille css bootstrap -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <!-- Chargement de la grille css bootstrap -->
     <link rel="stylesheet" href="assets/stylesheets/animate.css">
-    <link rel="stylesheet" href="assets/stylesheets/application.css">
+    <link rel="stylesheet" href="assets/stylesheets/dashboard.css">
     <title>Menu Principal | Play-Life</title>
 </head>
 <body>
-    
+    <div class="container-website-wallpaper">
+        <h1 class="title-website">Play-<span class="red">Life</span>.fr</h1>
+        <!-- Choix de depart -->
+        <div class="box-action">
+            <li class="action-select">Nouveau Profil</li>
+            <li class="action-select">Charger un Profil</li>
+            <li class="action-select">En savoir plus</li>
+            <li class="action-select">Micro-Serum</li>
+        </div>
+        <!-- Choix de depart -->
+
+        <!-- Nouveau Profil -->
+        <div class="box-new-profil">
+            
+        </div>
+        <!-- Nouveau Profil -->
+        <i class="copy">Une idée original | 2018 | Propulsé par <span class="red">Micro-Serum</span></i>
+        <!-- Interaction touch -->
+        <p class="interact-touch"><span class="touch">ESC</span> POUR ANNULER</p>
+        <!-- Interaction touch -->
+    </div>
 </body>
 <script src="assets/js/jquery.js"></script>
 <script src="assets/js/app.js"></script>
