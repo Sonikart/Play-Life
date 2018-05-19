78a6134410211f3c35c71d402aedebf2ea4c12b5
Sonik <dylan.lemaire@icloud.com>
2018-05-19 22:42:35 +0200

    Patch 1.0.1

--- public/index.php
+++ public/index.php
@@ -17,17 +17,14 @@
                                 if($level < 10) {
                                     $level = '0'.$level;
                                 }
-                                
                                 echo $level;
                             ?>
                         </div>
                         <div class="content-info-xp">
                             <div class="content-text-xp">
-                                Experience : <b><?= $infoplus['experience']; ?>%</b>
+                                Votre experience : <b><?= $infoplus['experience']; ?>%</b>
                             </div>
-                            <div class="content-xp">
-
-                            </div>
+                            <div class="content-xp"></div>
                         </div>
                     </div>
                     <div class="box-information-profil">
@@ -35,18 +32,39 @@
                             <div class="information-level">
                                 <p><i class="fas fa-trophy"></i> 03</p>
                             </div>
+                            <div class="information-plateform">
+                                <i class="fab fa-playstation icon-plateform"></i>
+                                <i class="fab fa-windows icon-plateform"></i>
+                            </div>
                             <img class="avatar_profil" src="<?= $site->get_config('link_site'); ?>assets/upload/avatar/<?= $infoplus['avatar'] ?>" alt="">
-                            <h4><?= $info['username']; ?></h4>
-                            <p class="tp16">Plateforme : <i class="fab fa-playstation"></i> <i class="fab fa-windows"></i></p>
+                            <h3><?= $info['username']; ?></h3>
                             <p class="tp12"><i>Joueur CSGO / H1Z1 / Fortnite, Je suis également developpeur indépendant en free-lance avec mon meilleur ami.</i></p>
+                            <div class="box-switch">
+                                <li class="switch-element"><i class="fas fa-briefcase"></i> Mon Inventaire (8)</li>
+                                <li class="switch-element"><i class="fas fa-cog"></i> Parametre</li>
+                            </div>
                         </div>
+                    </div>
+                    <div class="interaction-website">
+                        LA BOUTIQUE
                     </div>
                 </div>
                 <div class="col-md-6">
                     Salut salut salut saluts Salut salut salut saluts Salut salut salut saluts Salut salut salut saluts Salut salut salut saluts Salut salut salut saluts
                 </div>
                 <div class="col-md-3">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla, autem? Libero qui, tempora amet reiciendis nobis, fuga deserunt rem voluptates dolore tenetur quos quasi aliquam a, earum dicta ut alias.
+                    <img style="margin-bottom:10px;" src="assets/img/microserum.png" width="100%" alt="">
+                    <div class="box-warning">
+                        <div class="title-warning">
+                            <i class="fas fa-question-circle"></i> Information Systeme
+                        </div>
+                        <li class="box-content-warning"><strong>Systeme : </strong> L'ouverture de la bêta est programmer pour bientot.</li>
+                        <li class="box-content-warning"><strong>Systeme : </strong> Inscription fermer.</li>
+                        <li class="box-content-warning"><strong>Systeme : </strong> L'ouverture de la bêta est programmer pour bientot.</li>
+                        <li class="box-content-warning"><strong>Systeme : </strong> Inscription fermer.</li>
+                        <li class="box-content-warning"><strong>Systeme : </strong> L'ouverture de la bêta est programmer pour bientot.</li>
+                        <li class="box-content-warning"><strong>Systeme : </strong> Inscription fermer.</li>
+                    </div>
                 </div>  
             </div>
         </div>
